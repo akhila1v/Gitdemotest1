e940ba165dc3e6868c950928915820532a57fb4e
user <user@DESKTOP-I9H640F>
2022-12-14 23:23:19 +0530

    new commit

--- Gitdemo1/src/testing/TestDemo.java
+++ Gitdemo1/src/testing/TestDemo.java
@@ -3,7 +3,15 @@ package testing;
 public class TestDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		{
+		String hungry = "yes";
+		if (hungry == "yes")
+		System.out.println("eat something");
+		else
+		{
+		System.out.println("do work...");
+		}
+		
 
 	}
 
